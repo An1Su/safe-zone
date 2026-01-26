5a0c20b7ded1ecdbbec2059974c4efa9547b5203
Anastasia Suhareva <anastasija.suhareva@gritlab.ax>
2026-01-26 16:33:40 +0200

    fix: changed test, added no arg constructor

--- backend/shared/src/main/java/com/buyapp/common/event/UserEvent.java
+++ backend/shared/src/main/java/com/buyapp/common/event/UserEvent.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UserEvent implements Serializable {
     private static final long serialVersionUID = 1L;
 
